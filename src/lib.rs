fa39aa00cb626493cc02fc8b0eb64c90e8352a18
Violet Iapalucci <violet_iapalucci@outlook.com>
2024-04-08 12:18:30 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,47 @@
+use darling_packages as darling;
+
+static PACKAGE_MANAGER: Pacman = Pacman;
+
+struct Pacman;
+
+impl darling::PackageManager for Pacman {
+    fn name(&self) -> String {
+        "arch".to_owned()
+    }
+
+    fn install(
+        &self,
+        _context: &darling::Context,
+        package: &darling::InstallationEntry,
+    ) -> anyhow::Result<Option<String>> {
+        std::process::Command::new("pacman")
+            .arg("-S")
+            .arg(&package.name)
+            .spawn()?;
+        Ok(None)
+    }
+
+    fn uninstall(
+        &self,
+        _context: &darling::Context,
+        package: &darling::InstallationEntry,
+    ) -> anyhow::Result<()> {
+        std::process::Command::new("pacman")
+            .arg("-Rcns")
+            .arg(&package.name)
+            .spawn()?;
+        Ok(())
+    }
+
+    fn get_all_explicit(&self, _context: &darling::Context) -> anyhow::Result<Vec<String>> {
+        Ok(String::from_utf8(
+            std::process::Command::new("pacman")
+                .arg("-Qt")
+                .output()?
+                .stdout,
+        )?
+        .lines()
+        .map(|line| line.to_owned())
+        .collect())
+    }
+}
