bda060fb333cba1e66bba550208b6e61c045da0d
Violet Iapalucci <violet_iapalucci@outlook.com>
2024-04-08 21:25:09 -0400

    Silence stdout by default (to be configurable later)

--- src/lib.rs
+++ src/lib.rs
@@ -9,47 +9,29 @@ impl darling::PackageManager for Pacman {
         "arch".to_owned()
     }
 
-    fn install(
-        &self,
-        _context: &darling::Context,
-        package: &darling::InstallationEntry,
-    ) -> anyhow::Result<Option<String>> {
+    fn install(&self, _context: &darling::Context, package: &darling::InstallationEntry) -> anyhow::Result<Option<String>> {
         std::process::Command::new("sudo")
             .arg("pacman")
             .arg("-S")
+            .arg("--noconfirm")
             .arg(&package.name)
+            .stdout(std::process::Stdio::null())
             .status()?;
         Ok(None)
     }
 
-    fn uninstall(
-        &self,
-        _context: &darling::Context,
-        package: &darling::InstallationEntry,
-    ) -> anyhow::Result<()> {
-        std::process::Command::new("sudo")
-            .arg("pacman")
-            .arg("-Rcns")
-            .arg(&package.name)
-            .status()?;
+    fn uninstall(&self, _context: &darling::Context, package: &darling::InstallationEntry) -> anyhow::Result<()> {
+        std::process::Command::new("sudo").arg("pacman").arg("-Rcns").arg(&package.name).status()?;
         Ok(())
     }
 
-    fn get_all_explicit(
-        &self,
-        _context: &darling::Context,
-    ) -> anyhow::Result<Vec<(String, String)>> {
-        Ok(String::from_utf8(
-            std::process::Command::new("pacman")
-                .arg("-Qt")
-                .output()?
-                .stdout,
-        )?
-        .lines()
-        .map(|line| {
-            let splits = line.split(' ').collect::<Vec<_>>();
-            (splits[0].to_owned(), splits[1].to_owned())
-        })
-        .collect())
+    fn get_all_explicit(&self, _context: &darling::Context) -> anyhow::Result<Vec<(String, String)>> {
+        Ok(String::from_utf8(std::process::Command::new("pacman").arg("-Qt").output()?.stdout)?
+            .lines()
+            .map(|line| {
+                let splits = line.split(' ').collect::<Vec<_>>();
+                (splits[0].to_owned(), splits[1].to_owned())
+            })
+            .collect())
     }
 }
